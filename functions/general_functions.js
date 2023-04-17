a55530fcf7456d7db0815d03cb8e7caea74254df
SebastianAmariei <sebastianamariei02@gmail.com>
2023-04-18 00:27:14 +0100

    created the different endpoints for post that need to be implemented

---
+++ functions/general_functions.js
@@ -0,0 +1,22 @@
+/* File for useful functions to encourage DRY code */
+
+/* DRY secure postgreSQl query function */
+/* Example of how to use: pgQuery("INSERT INTO users (username, age, number) VALUES ($1, $2, $3)", "usernameValue", 25, 42) */
+async function pgQuery (query, ...inputs) {
+    const pgQuery = {
+        text: query,
+        values: inputs
+    }
+    
+    try {
+        const queryResult = await pool.query(pgQuery)
+        return queryResult
+    } catch (err) {
+        console.error('Error executing postgreSQL query:', err)
+        return {error: `There has been an error performing this query: ${err}`}
+    }
+}
+
+
+
+module.exports = {pgQuery}
--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,135 @@
 /* For video/image posting routes */
 
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const { pgQuery } = require('../functions/general_functions')
+
+const router = express.Router()
+
+
 
 /* Testing Posts Route */
 router.get("/testing", async (req, res) => {
   try {
-    res.json({ "Testing": "Working Posts" });
+    res.json({ "Testing": "Working Posts" })
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
-});
+})
+
+
+
+                  /* Home Page Paths */
+
+/* Get posts */
+router.get("/homepage/getposts", async (req, res) => {
+  try {
+
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Getting Ingredient For Specific Post */
+router.get("/homepage/getingredients", async (req, res) => {
+  try {
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Getting Comments For Specific Post */
+router.get("/homepage/getcomments", async (req, res) => {
+  try {
+
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Posting Message For Specific Post */
+router.post("/homepage/postmessage", async (req, res) => {
+  try {
+
+   
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Posting Reply For Specific Comment */
+router.get("/homepage/postreply", async (req, res) => {
+  try {
+
+ 
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Delete Message For Specific Post */
+router.delete("/homepage/deletemessage", async (req, res) => {
+  try {
+
+   
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Delete Reply For Specific Comment */
+router.delete("/homepage/deletereply", async (req, res) => {
+  try {
+
+ 
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Posting For Bookmarking Specific Post */
+router.post("/homepage/postbookmark", async (req, res) => {
+  try {
+
+ 
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Posting For Liking Specific Video */
+router.post("/homepage/postlike", async (req, res) => {
+  try {
+
+ 
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Deleting Like On Specific Video */
+router.delete("/homepage/deletelike", async (req, res) => {
+  try {
+
+ 
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+/* Posting For Sending Video To Friend */
+router.post("/homepage/postsendvideo", async (req, res) => {
+  try {
+
+ 
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+                /* Discover Page Paths */
 
 
 module.exports = router;
