5f52cb1338ab640a447f85af864575dbe4c94c6d
Mitchell <58375403+mitchellmclachlan@users.noreply.github.com>
2023-04-19 17:05:57 +0000

    new file: database.sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+/* An SQL file with all the SQL used so it is easier to move the database to another AWS account
+
+Command to connect to the database:
+psql --host=foodclub.cwlinpre6rr8.eu-north-1.rds.amazonaws.com --port=5432 --username=FoodCLUB123 --password --dbname=foodclub
+
+Password to the database:
+szqxrcjd */
+
+CREATE TABLE users (
+UserID SERIAL PRIMARY KEY,
+Username VARCHAR(255) UNIQUE
+);
