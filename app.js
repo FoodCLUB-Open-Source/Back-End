bedd6d6f9558f09badb17d65f13a9cb4a8766c32
jvan100 <j_van100@icloud.com>
2023-06-13 21:35:54 +0100

    Added base URL + development logging

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const requestLogging = require("./middleware/logging");
+
+const app = express();
+const router = express.Router();
+
+const SOCKET_ADDRESS = process.env.SOCKET_ADDRESS;
+
+/*  Middleware  */
+if (process.env.NODE_ENV !== "production") {
+	app.use(requestLogging);
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+	origin: [SOCKET_ADDRESS],
+	credentials: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*  All Routes  */
+router.use("/login", require("./routes/login"));
+router.use("/posts", require("./routes/posts"));
+router.use("/comments", require("./routes/comments"));
+router.use("/likes_views", require("./routes/likes_views"));
+
+const BASE_PATH = process.env.BASE_PATH;
+app.use(BASE_PATH, router);
+
+module.exports = app;
---
+++ middleware/logging.js
@@ -0,0 +1,15 @@
+const requestLogging = (req, res, next) => {	
+	const startMillis = Date.now();
+	const start = new Date(startMillis).toISOString();
+	const startDate = start.substring(0, 10);
+	const startTime = start.substring(11, 19);
+	
+	res.on("finish", () => {
+		const elapsedMillis = Date.now() - startMillis;		
+		console.log(`${req.method} ${startDate} ${startTime} ${res.statusCode} ${req.originalUrl} (${elapsedMillis} ms)`);
+	});
+	
+	next();
+}
+
+module.exports = requestLogging;
