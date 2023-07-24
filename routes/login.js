afbad6af99039bc385717040b0bc6b6576419a41
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-07-24 18:24:26 +0100

    Addressed review comments

--- routes/login.js
+++ routes/login.js
@@ -31,41 +31,35 @@ router.get("/testing", async (req, res) => {
     res.json({ "Testing": "Working Login" });
   } catch (err) {
     console.error(err.message);
+    res.json(err.message)
   }
 });
 
 /* Sign up */ 
 
 router.post('/signup', async (req, res) => {
-  var { user_id, username, email, phone_number, profilePicture, user_bio, gender, date_of_birth, password } = req.body;
+  const { username, email, phoneNumber, profilePicture, userBio, gender, dateOfBirth, password } = req.body;
   
 
-  if (!(username && email && date_of_birth && password)) {
-    res.status(400).json({ message :"Necessary input fields not given." });
+  if (!(username && email && dateOfBirth && password)) {
+    return res.status(400).json({ message :"Necessary input fields not given." });
   };
-  
-  var user_created_at_psql = new Date();
-  var user_updated_at_psql = user_created_at_psql;
 
   var attributeArray = [];
 
   /* aws cognito assigns a UUID value to each user's sub attribute */
   attributeArray.push(new AmazonCognitoId.CognitoUserAttribute({ Name: "email", Value: email }));
-  attributeArray.push(new AmazonCognitoId.CognitoUserAttribute({ Name: "phone_number", Value: phone_number }));
-  attributeArray.push(new AmazonCognitoId.CognitoUserAttribute({ Name: "birthdate", Value: date_of_birth }));
+  attributeArray.push(new AmazonCognitoId.CognitoUserAttribute({ Name: "phone_number", Value: phoneNumber }));  
 
-  var newUser = await appFunctions.pgQuery(`INSERT INTO users (username, email, phone_number, profile_picture, user_bio, gender, created_at, updated_at, date_of_birth) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-  username, email, phone_number, profilePicture, user_bio, gender, user_created_at_psql, user_updated_at_psql, date_of_birth);
-
-  userPool.signUp(username, password, attributeArray, null, (err, result) => {
+  userPool.signUp(username, password, attributeArray, null, async (err, result) => {
     if (err) {
       console.error(err);
       return res.status(400).json({message: err.message});
     }
-    cognitoUser = result.user;
-    return res.status(201).json({user: cognitoUser});
+    const newUser = await appFunctions.pgQuery(`INSERT INTO users (username, email, phone_number, profile_picture, user_bio, gender, created_at, updated_at, date_of_birth) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
+    username, email, phoneNumber, profilePicture, userBio, gender, new Date(), new Date(), dateOfBirth);
+    return res.status(201).json({user: result.user});
   });  
-
 })
 
 module.exports = router;
