6cb0afc67ec416ffb625f231c0c2ede5667fd6d6
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-07-29 12:18:22 +0100

    signin route

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,26 @@
-/* For login system routes */
-
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
+const http = require("http");
+const { pgQuery } = require("../functions/general_functions");
+const AmazonCognitoId = require("amazon-cognito-identity-js");
+const AWS = require('aws-sdk/global');
+
+const crypto = require('crypto')
+const bcrypt = require('bcrypt')
+const appFunctions = require('../functions/general_functions')
+const rateLimiter = require('../middleware/rate_limiter')
+
+const poolData = {
+  UserPoolId: "eu-west-2_KdYKyXzvR",
+  ClientId: "3oga7396va7mjl4qsemqk0at7u"
+};
+
+const pool_region = "eu-west-2";
+
+const userPool = new AmazonCognitoId.CognitoUserPool(poolData);
+
+router.use(bodyParser.urlencoded({ extended: false }))
 
 /* Testing Login Route */
 router.get("/testing", async (req, res) => {
@@ -9,8 +28,41 @@ router.get("/testing", async (req, res) => {
     res.json({ "Testing": "Working Login" });
   } catch (err) {
     console.error(err.message);
+    res.json(err.message)
   }
 });
 
+/* Sign in */
 
-module.exports = router;
+router.post('/signin', (req, res) => {
+  var username = req.body.username;
+  
+  var authenticationDetails = new AmazonCognitoId.AuthenticationDetails({
+    Username: username,
+    Password: req.body.password
+  });
+
+  var userData = {
+    Username: username,
+    Pool: userPool
+  };
+
+  var cognitoUser = new AmazonCognitoId.CognitoUser(userData);
+  
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: (result) =>{
+      var idToken = result.getIdToken().getJwtToken();
+      var accessToken = result.getAccessToken().getJwtToken();
+      var refreshToken = result.getRefreshToken().getToken();
+      res.status(200).json({ message: 'user authenticated successfully' });
+      AWS.config.region = pool_region;
+    },
+    onFailure: (err) => {
+      res.status(400).json({
+        header: 'incorrect sign-in details',
+        message: err.message});
+    }
+  });  
+});
+
+module.exports = router
