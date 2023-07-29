71488fcff465d0b356d6bba7680e77f3a3884e6c
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-07-29 10:15:32 +0100

    Comments addressed

--- routes/login.js
+++ routes/login.js
@@ -13,11 +13,9 @@ const bcrypt = require('bcrypt')
 const appFunctions = require('../functions/general_functions')
 const rateLimiter = require('../middleware/rate_limiter')
 
-const CognitoUserPool = AmazonCognitoId.CognitoUserPool;
-
 const poolData = {
-  UserPoolId: "eu-west-2_CJCWF6vna",
-  ClientId: "6ttdjdp51nocb2jn1it280squl"
+  UserPoolId: "eu-west-2_KdYKyXzvR",
+  ClientId: "3oga7396va7mjl4qsemqk0at7u"
 };
 
 const pool_region = "eu-west-2";
@@ -39,28 +37,35 @@ router.get("/testing", async (req, res) => {
 /* Sign up */ 
 
 router.post('/signup', rateLimiter(10, 1), async (req, res) => {
-  const { username, email, phoneNumber, profilePicture, userBio, gender, dateOfBirth, password } = req.body;
+  const { username, email, password } = req.body;
   
 
-  if (!(username && email && dateOfBirth && password)) {
+  if (!(username && email && password)) {
     return res.status(400).json({ message :"Necessary input fields not given." });
   };
 
   var attributeArray = [];
+  const passwordPlaceholder = NaN
+  const dateOfBirth = "01/01/2000"
 
   /* aws cognito assigns a UUID value to each user's sub attribute */
   attributeArray.push(new AmazonCognitoId.CognitoUserAttribute({ Name: "email", Value: email }));
-  attributeArray.push(new AmazonCognitoId.CognitoUserAttribute({ Name: "phone_number", Value: phoneNumber }));  
 
   userPool.signUp(username, password, attributeArray, null, async (err, result) => {
     if (err) {
       console.error(err);
       return res.status(400).json({message: err.message});
     }
-    const newUser = await appFunctions.pgQuery(`INSERT INTO users (username, email, phone_number, profile_picture, user_bio, gender, created_at, updated_at, date_of_birth) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-    username, email, phoneNumber, profilePicture, userBio, gender, new Date(), new Date(), dateOfBirth);
+    try {
+      const newUser = await appFunctions.pgQuery(`INSERT INTO users (username, email, password, date_of_birth) VALUES ($1, $2, $3, $4) RETURNING *`,
+      username, email, passwordPlaceholder, dateOfBirth);
+    } catch (error) {
+      return res.status(400).json(error.message)
+    }
     return res.status(201).json({user: result.user});
   });  
 })
+
+
 
 module.exports = router;
