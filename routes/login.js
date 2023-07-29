dd2315d6d73c8cb946940c862f45d0b28c41ccc5
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-07-29 10:37:56 +0100

    confirm verification code route

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,27 @@
 
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
+const http = require("http");
+const { pgQuery } = require("../functions/general_functions");
+const AmazonCognitoId = require("amazon-cognito-identity-js");
+const AWS = require('aws-sdk');
+
+const crypto = require('crypto')
+const bcrypt = require('bcrypt')
+const appFunctions = require('../functions/general_functions')
+const rateLimiter = require('../middleware/rate_limiter')
+
+const poolData = {
+  UserPoolId: "eu-west-2_KdYKyXzvR",
+  ClientId: "3oga7396va7mjl4qsemqk0at7u"
+};
+
+const pool_region = "eu-west-2";
+
+const userPool = new AmazonCognitoId.CognitoUserPool(poolData);
+
+router.use(bodyParser.urlencoded({ extended: false }))
 
 /* Testing Login Route */
 router.get("/testing", async (req, res) => {
@@ -11,6 +32,23 @@ router.get("/testing", async (req, res) => {
     console.error(err.message);
   }
 });
+
+/* Confirm verification code */
+router.post('/confirmverification', rateLimiter(10, 1), (req, res) => {
+  var userData = {
+    Username: req.body.username,
+    Pool: userPool,
+  };
+
+  var cognitoUser = new AmazonCognitoId.CognitoUser(userData);
+
+  cognitoUser.confirmRegistration(req.body.verificationCode, true, (err, result) => {
+    if (err) {
+      return res.status(400).json(err.message)
+    }
+    return res.status(201).json({message: 'user verified'})
+  });
+})
 
 
 module.exports = router;
