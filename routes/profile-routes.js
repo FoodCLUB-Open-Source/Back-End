4e657c9c903deaca587d53966ed36f7b214722db
mr284 <mr284@st-andrews.ac.uk>
2023-08-16 01:42:07 +0530

    Completed task 16

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -30,4 +30,29 @@ router.get("/profile/:user_id/following", rateLimiter(), inputValidator, async (
     }
 });
 
+/**
+ * Retrieves users that follow the user
+ * 
+ * @route GET /profile/:userid/followers
+ * @param {string} req.params.user_id - The ID of the user to retrieve users that follow the user
+ * @param {string} req.query.pageNumber - The pageNumber for pagination
+ * @param {string} req.query.pageSize - The pageSize for pagination
+ * @returns {Object} - An object containing details of the users that follow the user such as id, username and profile picture
+ * @throws {Error} - If there is error retrieving user details or validation issues
+ */
+router.get("/profile/:user_id/followers", rateLimiter(), inputValidator, async (req, res) => {
+    try {
+        const userID = req.params.user_id; // getting userID
+        const { pageNumber, pageSize } = req.query; // getting page number and page size
+
+        const query = 'SELECT following.user_id, users.username, users.profile_picture FROM following JOIN users on following.user_id = users.id WHERE following.user_following_id = $1 ORDER BY following.created_at ASC LIMIT $3 OFFSET (($2 - 1) * $3)'; // returns the users that follow the user with pagination
+        const userFollowers = await pgQuery(query, userID, pageNumber, pageSize);
+
+        return res.status(200).json({ data: userFollowers.rows }); // sends details to client
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: error.message }); // server side error
+    }
+});
+
 export default router;
