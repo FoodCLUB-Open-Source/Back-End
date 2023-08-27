7d891e65cc88590dbfc2ef7754dd96943d91892b
SebastianAmariei <sebastianamariei02@gmail.com>
2023-08-27 12:27:28 +0100

    sign up clean-up and comments added

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -21,8 +21,17 @@ router.get("/testing", async (req, res) => {
   }
 });
 
-/* Sign up */ 
-router.post('/signup', inputValidator, rateLimiter(10, 1), async (req, res) => {
+/**
+ * Sign up a user
+ * 
+ * @route POST /login/signup
+ * @body {string} req.body.username - Users Username
+ * @body {string} req.body.email - Users email
+ * @body {string} req.body.password - Users password
+ * @returns {status} - A status indicating successful sign up
+ * @throws {Error} - If there are errors Dont create user.
+ */
+router.post('/signup', inputValidator, rateLimiter(), async (req, res) => {
   
   const { username, email, password } = req.body;
   
@@ -31,7 +40,7 @@ router.post('/signup', inputValidator, rateLimiter(10, 1), async (req, res) => {
   }
 
   const attributeArray = [];
-  const passwordHashed = await hash(password, 10)
+  const passwordHashed = await hash(password, 10);
 
   /* aws cognito assigns a UUID value to each user's sub attribute */
   attributeArray.push(new CognitoUserAttribute({ Name: "email", Value: email }));
@@ -45,16 +54,24 @@ router.post('/signup', inputValidator, rateLimiter(10, 1), async (req, res) => {
       await pgQuery(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`,
       username, email, passwordHashed);
     } catch (error) {
-      return res.status(400).json(error.message)
+      return res.status(400).json(error.message);
     }
     return res.status(201).json({user: result.user});
   });  
 });
 
-/* Confirm verification code */
-router.post('/confirmverification', inputValidator, rateLimiter(10, 1), (req, res) => {
+/**
+ * Verify a users verification code after sign up.
+ * 
+ * @route POST /login/confirm_verification
+ * @body {string} req.body.username - Users Username
+ * @body {string} req.body.verificationCode - Verification code from users email
+ * @returns {status} - A successful status indicates code verfified
+ * @throws {Error} - If there are errors dont verify code
+ */
+router.post('/confirm_verification', inputValidator, rateLimiter(), (req, res) => {
 
-  const {username, verificationCode} = req.body;
+  const {username, verification_code} = req.body;
 
   const userData = {
     Username: username,
@@ -63,16 +80,23 @@ router.post('/confirmverification', inputValidator, rateLimiter(10, 1), (req, re
 
   const cognitoUser = new CognitoUser(userData);
 
-  cognitoUser.confirmRegistration(verificationCode, true, (err, result) => {
+  cognitoUser.confirmRegistration(verification_code, true, (err, result) => {
     if (err) {
-      return res.status(400).json(err.message)
+      return res.status(400).json(err.message);
     }
-    return res.status(201).json({message: 'user verified'})
+    return res.status(201).json({message: 'user verified'});
   });
 });
 
-/* Resend Verification Code */
-router.post('/resendverificationcode',inputValidator, rateLimiter(10, 1), (req, res) => {
+/**
+ * Send another verification code to user
+ * 
+ * @route POST /login/resend_verification_code
+ * @body {string} req.body.username - Users Username
+ * @returns {status} - A successful status indicates code resent
+ * @throws {Error} - If there are errors dont send another verififcation code
+ */
+router.post('/resend_verification_code', inputValidator, rateLimiter(), (req, res) => {
 
   const { username } = req.body;
 
@@ -91,9 +115,16 @@ router.post('/resendverificationcode',inputValidator, rateLimiter(10, 1), (req, 
   });
 });
 
- /* Sign in */
-
-router.post('/signin',inputValidator, rateLimiter(10,1), (req, res) => {
+/**
+ * Allows a user to sign in to their account
+ * 
+ * @route POST /login/signin
+ * @body {string} req.body.username - Users Username
+ * @body {string} req.body.password - Users password
+ * @returns {status} - A successful status indicates successful sign in
+ * @throws {Error} - If there are errors dont sign user in
+ */
+router.post('/signin', inputValidator, rateLimiter(), (req, res) => {
 
   const {username, password} = req.body;
   
@@ -111,8 +142,7 @@ router.post('/signin',inputValidator, rateLimiter(10,1), (req, res) => {
   
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: async (result) =>{
-      const user = await pgQuery('SELECT id, username, profile_picture FROM users WHERE username = $1', username)
-      console.log(user.rows)
+      const user = await pgQuery('SELECT id, username, profile_picture FROM users WHERE username = $1', username);
       res.status(200).json({ user: user.rows[0] });
     },
     onFailure: (err) => {
@@ -123,10 +153,14 @@ router.post('/signin',inputValidator, rateLimiter(10,1), (req, res) => {
   });  
 });
 
-
-/* Sign out of application*/
-
-router.post('/signout', rateLimiter(10,1), (req, res) => {
+/**
+ * Sign a user out
+ * 
+ * @route POST /login/signout
+ * @returns {status} - A successful status means sign out successful
+ * @throws {Error} - If there are errors dont sign a user out
+ */
+router.post('/signout', rateLimiter(), (req, res) => {
 
   const cognitoUser = cognitoUserPool.getCurrentUser();
 
@@ -138,21 +172,28 @@ router.post('/signout', rateLimiter(10,1), (req, res) => {
   };
 });
 
-/* Change password */
+/**
+ * Changes a users password from old to new
+ * 
+ * @route POST /login/change_password
+ * @body {string} req.body.oldPassword - Users old password
+ * @body {string} req.body.newPassword - Users new password
+ * @returns {status} - A successful status indicates password successfully changed
+ * @throws {Error} - If there are errors dont change the users passwords
+ */
+router.post('/change_password', inputValidator, rateLimiter(), async (req, res) => {
 
-router.post('/changepassword',inputValidator, rateLimiter(10, 1), async (req, res) => {
+  const {old_password, new_password} = req.body;
 
-  const {oldPassword, newPassword} = req.body;
-
-  const cognitoUser = cognitoUserPool.getCurrentUser()  
+  const cognitoUser = cognitoUserPool.getCurrentUser();
 
   cognitoUser.getSession((err, session) => {
     if (err) {
-      return res.status(400).json(err.message)
+      return res.status(400).json(err.message);
     }
   });
   
-  cognitoUser.changePassword(oldPassword, newPassword, (err, result) => {
+  cognitoUser.changePassword(old_password, new_password, (err, result) => {
     if (err) {
       return res.status(400).json(err.message);
     }
@@ -160,7 +201,17 @@ router.post('/changepassword',inputValidator, rateLimiter(10, 1), async (req, re
   });
 });
 
-router.post('/forgotpasswordcode', async (req, res) => {
+
+/**
+ * Sends a user a forgot password verification code
+ * 
+ * @route POST /login/forgot_password/verification_code
+ * @body {string} req.body.username - Users Username
+ * @returns {status} - A successful status indicates code is sent
+ * @throws {Error} - If there are errors dont send a code
+ */
+router.post('/forgot_password/verification_code', async (req, res) => {
+
   const { username } = req.body;
 
   const userData = {
@@ -175,13 +226,24 @@ router.post('/forgotpasswordcode', async (req, res) => {
       res.status(200).json({ message: 'Verification code sent' });
     },
     onFailure: (err) => {
-      res.status(400).json(err.message)
+      res.status(400).json(err.message);
     },
   });
 });
 
-router.post('/forgotpasswordcode/newpassword', (req, res) => {
-  const { username, verificationCode, newPassword } = req.body;
+/**
+ * Uses verification code to  change the password
+ * 
+ * @route POST /login/forgot_password_code/new_password
+ * @body {string} req.body.username - Users Username
+ * @body {string} req.body.verification_code - Verification code that was sent
+ * @body {string} req.body.newPassword - Users new password
+ * @returns {status} - A successful status indicates new password has been set
+ * @throws {Error} - If there are errors dont chagne the password
+ */
+router.post('/forgot_password_code/new_password', (req, res) => {
+
+  const { username, verification_code, new_password } = req.body;
 
   const userData = {
     Username: username,
@@ -190,18 +252,25 @@ router.post('/forgotpasswordcode/newpassword', (req, res) => {
 
   const cognitoUser = new CognitoUser(userData);
 
-  cognitoUser.confirmPassword(verificationCode, newPassword, {
+  cognitoUser.confirmPassword(verification_code, new_password, {
     onSuccess() {
-      res.status(201).json({ message: 'password reset successfully'})
+      res.status(201).json({ message: 'password reset successfully'});
     },
     onFailure(err) {
       res.status(400).json(err.message);
     },
   });
 });
-/* Global signout invalidates all user tokens */ 
 
-router.post('/globalsignout', rateLimiter(10, 1), (req, res) => {
+
+/**
+ * Send another verification code to user
+ * 
+ * @route POST /login/global_signout
+ * @returns {status} - A successful status indicates user is signed out on all devices he is logged in on
+ * @throws {Error} - If there are errors dont sign user out on any device
+ */
+router.post('/global_signout', rateLimiter(), (req, res) => {
 
   const cognitoUser = cognitoUserPool.getCurrentUser();
 
@@ -213,9 +282,9 @@ router.post('/globalsignout', rateLimiter(10, 1), (req, res) => {
   }
 });
 
-/* Delete a user */
- 
-router.post('/deleteuser', rateLimiter(10, 1), (req, res) => {
+
+//Change this to a function to be used in the profile Routes.
+router.delete('/delete_user', rateLimiter(10, 1), (req, res) => {
 
   const username = req.body.username;
 
@@ -234,8 +303,8 @@ router.post('/deleteuser', rateLimiter(10, 1), (req, res) => {
   });
 });
 
-
-router.post('/update/:attribute', (req, res) => {
+//Change this to a function to so that it can be used in profile routes to delete user.
+router.put('/update/:attribute', (req, res) => {
 
   const { attribute } = req.params;
   const { newUserAttribute } = req.body;
