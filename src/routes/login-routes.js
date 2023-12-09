608f84cb4e26eb49cda3a8dfed2454c2333a2195
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-12-09 13:25:53 +0000

    removed token error

--- src/routes/login-routes.js
+++ src/routes/login-routes.js
@@ -166,7 +166,7 @@ router.post("/resend_verification_code",inputValidator,rateLimiter(),(req, res) 
   
   cognitoUser.resendConfirmationCode((err, result) => {
     if (err) {
-      return res.status(400).json({ message: err.msg })
+      return res.status(400).json({ message: err.message })
     }
     res.status(200).json({ 
       header: 'User email is not confirmed',
@@ -210,7 +210,7 @@ router.post("/signin",inputValidator,rateLimiter(),emailOrUsername(),(req, res) 
       if (err.message == "User is not confirmed.") {
         cognitoUser.resendConfirmationCode((err, result) => {
           if (err) {
-            return res.status(400).json({ message: err.msg })
+            return res.status(400).json({ message: err.message })
           }
           res.status(400).json({ 
             message: 'User is not verified',
@@ -278,29 +278,25 @@ router.post("/signout", rateLimiter(), (req, res) => {
  * @route POST /login/change_password
  * @body {string} req.body.old_password - Users old password
  * @body {string} req.body.new_password - Users new password
+ * @body {string} req.body.username - the user's username
  * @returns {status} - A successful status indicates password successfully changed
  * @throws {Error} - If there are errors dont change the users passwords
  */
 router.post("/change_password", inputValidator, rateLimiter(), (req, res) => {
-  const { old_password, new_password } = req.body;
+  const { username, old_password, new_password } = req.body;
+  
+  const userData = {
+    Username: username,
+    Pool: cognitoUserPool,
+  };
 
+  const cognitoUser = new CognitoUser(userData);
 
-  getUserFromTokens((err, result) => {
+  cognitoUser.changePassword(old_password, new_password, (err, result) => {
     if (err) {
-      return res.status(400).json(err);
-    } else if (result) {
-      const cognitoUser = result;
-      console.log(cognitoUser);
-
-      cognitoUser.changePassword(old_password, new_password, (err, result) => {
-        if (err) {
-          return res.status(400).json({ message: err.msg });
-        }
-        return res
-          .status(201)
-          .json({ message: "password changed successfully" });
-      });
+      return res.status(400).json({ message: err.message });
     }
+    return res.status(201).json({ message: "password changed successfully" });
   });
 });
 
@@ -327,7 +323,7 @@ router.post("/forgot_password/verification_code",inputValidator,rateLimiter(),em
         res.status(200).json({ message: "Verification code sent" });
       },
       onFailure: (err) => {
-        res.status(400).json({ message: err.msg });
+        res.status(400).json({ message: err.message });
       },
     });
   }
@@ -382,7 +378,7 @@ router.post("/global_signout", rateLimiter(), (req, res) => {
 
       cognitoUser.deleteUser(async (err, result) => {
         if (err) {
-          return res.status(400).json({ message: err.msg });
+          return res.status(400).json({ message: err.message });
         }
         try {
           await pgQuery("DELETE FROM users WHERE username = $1", username);
@@ -462,7 +458,7 @@ router.delete('/delete_user', rateLimiter(), (req, res) => {
 
       cognitoUser.deleteUser(async (err, result) => {
         if (err) {
-          return res.status(400).json({ message: err.msg });
+          return res.status(400).json({ message: err.message });
         }
         try {
           await pgQuery("DELETE FROM users WHERE username = $1", username);
