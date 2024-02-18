e9d318607a9c7561f9924bf213869c25e10d4d0f
Jordan H. Bugai <jordanhbugai@gmail.com>
2024-02-17 19:57:34 -0700

    Added JSDoc documentation to verify.js

--- src/middleware/verify.js
+++ src/middleware/verify.js
@@ -3,13 +3,14 @@ import { accessVerifier, idVerifier }from "../config/cognito.js";
 import { parseHeader, parseHeaderAccess } from "../functions/cognito_functions.js";
 
 // All tokens should be passed from frontend and stored as strings, not as objects.
-// Verifier that expects valid access tokens:
-/**
- * INTERNAL USE ONLY
- * @param {*} accessToken 
- * @returns 
- */
 
+/** 
+ * Function that verifies access token
+ * INTERNAL USE ONLY
+ * 
+ * @param {*} accessToken - Token to be verified by middleware
+ * @throws {Error} - If access token is invalid or null
+ */
 export const verifyAccessToken = async (accessToken) => {
 
   if (!!accessToken) {
@@ -29,13 +30,14 @@ export const verifyAccessToken = async (accessToken) => {
   };
 };
 
-// Function to verify Id token
-/** INTERNAL USE ONLY
+/**
+ * Function to verify Id token
+ * INTERNAL USE ONLY
  * 
- * @param {*} idToken 
- * @returns 
+ * @param {*} idToken - ID token to be ferified 
+ * @returns {object} - Object with user ID and username 
+ * @throws {Error} - If ID token is null or invalid 
  */
-
 export const verifyIdToken = async (idToken) => {
 
   if (!!idToken) {
@@ -61,10 +63,12 @@ export const verifyIdToken = async (idToken) => {
 
 
 /**
- * Middleware for verification of both access and id tokens, to be used with endpoints that accept both.
+ * Middleware funciton for verification of both access and id tokens, to be used with endpoints that accept both
+ * 
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
+ * @returns {status} - next() if successful, otherwise 404 with error message
  */
 export const verifyTokens = async (req, res, next) => {
   try {
@@ -81,11 +85,13 @@ export const verifyTokens = async (req, res, next) => {
 };
 
 /**
- * Middleware for authenticating only the access token, for endpoints that only require the access token.
- * Endpoints that only require the access token will be endpoints that do not require any user information.
+ * Middleware function for authenticating only the access token, for endpoints that only require the access token
+ * Endpoints that only require the access token will be endpoints that do not require any user information
+ * 
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
+ * @returns {status} - next() if successful, otherwise 404 with error message
  */
 export const verifyAccessOnly = async (req, res, next) => {
   try {
@@ -100,11 +106,12 @@ export const verifyAccessOnly = async (req, res, next) => {
 };
 
 /**
- * Middleware for authenticating both the access token and the id token, but which does not change the request body.
+ * Middleware function for authenticating both the access token and the id token, but which does not change the request body
+ * 
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
- * @returns 
+ * @returns {status} - next() if successful, otherwise 404 with error message
  */
 export const verifyUserIdentity = async (req, res, next) => {
   try {
