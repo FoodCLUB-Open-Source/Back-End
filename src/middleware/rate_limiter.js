8a60371d87acd245d2cd16c92d36650255590c15
Jordan H. Bugai <jordanhbugai@gmail.com>
2024-02-17 19:34:19 -0700

    Added JSDoc documentation to rate_limiter.js

--- src/middleware/rate_limiter.js
+++ src/middleware/rate_limiter.js
@@ -1,14 +1,18 @@
 /* This file contains the rate limiter that will be used throughout the app */
 import redis from "../config/redisConfig.js";
 
-/* 
-    This is the middleware that will be implemented in the following manner:
+/**
+  This is the middleware that will be implemented in the following manner:
     app.use('/api/fast', rateLimiter(200, 15), (req, res) => {
 
-    or alternatively:
+  or alternatively:
 
     you can just use rateLimiter() to use the default values that are specified in the .env file
-*/
+ * 
+ * @param {*} maxRequests - Maximum number of requests 
+ * @param {*} windowInMins 
+ * @returns {status} - Unsuccessful status if rate limit exceeded, otherwise successful if no errors
+ */
 const rateLimiter = (maxRequests, windowInMins) => {
   return async (req, res, next) => {
     try {
