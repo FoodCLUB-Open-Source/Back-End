456402326be22deff6ff7423c1f5ed2b87cb0750
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-03-21 14:41:50 +0000

    bookmark query resolved

--- src/routes/bookmarks-routes.js
+++ src/routes/bookmarks-routes.js
@@ -27,7 +27,7 @@ router.delete("/profile/bookmark/:post_id", rateLimiter(), verifyTokens, inputVa
       res.status(200).json({ message: "Post is no longer bookmarked" }); // if true success response code is sent
     }
     else if (postgresQuery.rowCount === 0) {
-      res.status(400).json({ message: "This post has already been deleted" }); 
+      res.status(400).json({ message: "This post has already been deleted" });
     }
     else {
       res.status(400).json({ message: postgresQuery.error }); // else unsuccessful response code is sent along with error message
@@ -72,38 +72,42 @@ router.post("/post/bookmark/:post_id", rateLimiter(), verifyTokens, inputValidat
  * @returns {status} - If successful, returns 200 and an array of the posts bookmarked by the user
  * @throws {Error} - If there are errors fetching bookmarked posts
  */
-router.get("/:user_id", rateLimiter(), inputValidator, async(req, res, next) => {
+router.get("/:user_id", rateLimiter(), inputValidator, async (req, res, next) => {
   try {
     const { user_id } = req.params; // retrieving userID
     const { page_number, page_size } = req.query; // getting page number and page size
 
     // const bookmarkPostsQuery = "SELECT p.id, p.title, p.description, p.video_name, p.thumbnail_name, p.created_at FROM posts p JOIN bookmarks b ON p.id = b.post_id WHERE b.user_id = $1 ORDER BY b.created_at DESC LIMIT $3 OFFSET (($2 - 1) * $3)"; // query to get bookmarked post details
     const bookmarkPostsQuery = `
-        SELECT 
-            p.id, p.title, p.description, p.video_name, p.thumbnail_name, p.created_at, u.id AS user_id, u.full_name, u.profile_picture, u.username, pc.name as post_category,
-            CASE 
-                WHEN fol.user_following_id IS NOT NULL THEN true 
-                ELSE false 
-            END as isFollowed
-        FROM 
-            posts p 
-        JOIN 
-            bookmarks b ON p.id = b.post_id 
-        JOIN 
-            users u ON p.user_id = u.id
-        JOIN
-            posts_categories pc ON pc.post_id = p.id
-        LEFT JOIN
-            following fol ON u.id = fol.user_following_id AND fol.user_id = $1
-        WHERE 
-            b.user_id = $1 
-        ORDER BY 
-            b.created_at DESC 
-        LIMIT $3 
-        OFFSET (($2 - 1) * $3)`; // query to get bookmarked post details
+    SELECT 
+        p.id, p.title, p.description, p.video_name, p.thumbnail_name, p.created_at, 
+        u.id AS user_id, u.full_name, u.profile_picture, u.username, pc.category_name AS post_category,
+        CASE 
+            WHEN fol.user_following_id IS NOT NULL THEN true 
+            ELSE false 
+        END AS isFollowed
+    FROM 
+        posts p 
+    JOIN 
+        bookmarks b ON p.id = b.post_id 
+    JOIN 
+        users u ON p.user_id = u.id
+    JOIN
+        posts_categories pc ON pc.post_id = p.id
+    LEFT JOIN
+        following fol ON u.id = fol.user_following_id AND fol.user_id = $1
+    WHERE 
+        b.user_id = $1 
+    ORDER BY 
+        b.created_at DESC 
+    LIMIT $3 
+    OFFSET (($2 - 1) * $3)
+`;
+
 
     const bookmarkPostsQueryPromise = await pgQuery(bookmarkPostsQuery, user_id, page_number, page_size);
-    const updatedPostsData = await updatePosts(bookmarkPostsQueryPromise.rows,parseInt(user_id)); // updating post objects to include further information
+
+    const updatedPostsData = await updatePosts(bookmarkPostsQueryPromise.rows, parseInt(user_id)); // updating post objects to include further information
 
     return res.status(200).json({ data: updatedPostsData }); // sending data to client (if array is empty it means user has no posts bookmarked or posts information does not exist in database)
   } catch (error) {
