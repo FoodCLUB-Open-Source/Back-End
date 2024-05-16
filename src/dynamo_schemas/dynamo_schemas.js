4b6a37fceafbbabc6b314d1af2313629f025aa27
Dozie2001 <davidchidozie402@gmail.com>
2024-05-16 13:09:43 +0100

    Updated Schema

--- src/dynamo_schemas/dynamo_schemas.js
+++ src/dynamo_schemas/dynamo_schemas.js
@@ -43,15 +43,27 @@ export const setStoryViews = (storyId, userId) => ({
   created_at: new Date().toISOString()
 });
 
-export const setStory = (userId, imageUrl, thumbnailUrl, store_in_memory) => ({
-  story_id: uuid(),
-  user_id: userId,
-  imageUrl: imageUrl,
-  thumbnail_url: thumbnailUrl,
-  view_count: 0,
-  created_at: new Date().toISOString(),
-  store_in_memory: store_in_memory
-});
+export const setStory = (userId, imageUrl, thumbnailUrl, store_in_memory) => {
+  const story = {
+    story_id: uuid(),
+    user_id: userId,
+    imageUrl: imageUrl,
+    thumbnail_url: thumbnailUrl,
+    view_count: 0,
+    created_at: new Date().toISOString(),
+    store_in_memory: store_in_memory,
+  };
+
+  if (!store_in_memory) {
+    const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+    const ttlExpiration = currentTimeInSeconds + (24 * 60 * 60);
+    story.TTL = ttlExpiration;
+  }
+
+  return story;
+};
+
+
 export const setStoryReactions = (story_id, user_id, reaction_Id) => ({
   story_id: story_id,
   user_id, user_id,
